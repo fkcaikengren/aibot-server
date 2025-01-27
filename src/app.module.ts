d190f42ad3efcdddcf0b3f1ed0bc6adedd6fe522
caikengren <fkcaikengren@163.com>
2025-01-27 19:10:59 +0800

    fix: 修改redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -69,7 +69,7 @@ function getEnvFilePath() {
             host: process.env.REDIS_HOST || 'localhost',
             port: parseInt(process.env.REDIS_PORT) || 6379,
           },
-          password: process.env.REDIS_PASSWORD,
+          // password: process.env.REDIS_PASSWORD,
         }),
     }),
     JwtModule.register({
